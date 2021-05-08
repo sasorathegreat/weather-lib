43b996fcf6a8dcd432b1dc7587528dc9dca09c81
Crystal Logan <crystal_d_logan@outlook.com>
2021-05-08 18:29:09 -0400

    Adding Object mappers for mapping json results to an object

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 // Importing packages
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,6 +16,24 @@ func main() {
 	fmt.Println("Hello, world!")
 	fmt.Println(Get("28027"))
 	//result = Get("28027")
+}
+
+// A Response struct to map the Entire Response
+type Response struct {
+	Main Main `json:"main"`
+	Wind Wind `json:"wind"`
+}
+
+// A Wind Struct to map wind.
+type Wind struct {
+	Speed string `json:"speed"`
+	Deg   string `json:"deg"`
+}
+
+// A Main Struct to map main.
+type Main struct {
+	Temp     string `json:"temp"`
+	Humidity string `json:"humidity"`
 }
 
 func Get(zipcode string) string {
@@ -31,5 +50,11 @@ func Get(zipcode string) string {
 		log.Fatal(err)
 	}
 	fmt.Println(string(responseData))
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject.Main.Temp)
+	fmt.Println(responseObject.Wind.Speed)
 	return value
 }
